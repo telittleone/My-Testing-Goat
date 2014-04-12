822c6b2c05c2c7feb81526308565f1d371ee97ef
vagrant <vagrant@precise32.(none)>
2014-04-12 08:17:05 +0000

    Model for list Items and associated migration

---
+++ lists/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# encoding: utf8
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
